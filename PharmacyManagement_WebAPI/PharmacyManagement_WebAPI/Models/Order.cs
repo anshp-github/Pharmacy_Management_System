a5b64a265385a165a4a60df835c2a44ec6fa6399
ayushi-das <ayushidas503@gmail.com>
2022-09-25 22:22:48 +0530

    Validation updated

--- PharmacyManagement_WebAPI/PharmacyManagement_WebAPI/Models/Order.cs
+++ PharmacyManagement_WebAPI/PharmacyManagement_WebAPI/Models/Order.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace PharmacyManagement_WebAPI.Models
 {
     public class Order
     {
         public int OrderId { get; set; }
+        [Required(ErrorMessage = "Date is requierd!")]
+        [Display(Name = "Date")]
         public DateTime OrderDate { get; set; }
         public bool IsPickedUp { get; set; } = false;
+        [Required(ErrorMessage = "Amount is requierd!")]
+        [Display(Name = "Amount")]
         public int Amount { get; set; }
         public int Count { get; set; }
         public int DoctorId { get; set; }
